a9867f4429ed92666fa68b4d54f46cb875dd695a
maive32 <94019913+maive32@users.noreply.github.com>
2022-03-22 21:23:40 -0700

    d

--- main.js
+++ main.js
@@ -143,11 +143,33 @@ runRobot(VillageState.random(),routeRobot,[]);
   
   compareRobots(routeRobot, [], goalOrientedRobot, []);
   // Robot efficiency 
-
+  
 
   // Persistent group
   class PGroup {
+    constructor() {
+      this.members = members;
+    }
 
+    add(value) {
+      if (this.has(value)) return this; 
+      return new PGroup(this.members.concat([value]));
+    }
+
+    delete(value) {
+      if (!this.has(value)) return this; 
+      return new PGroup(this.members.filter(m => m !== value));
+    }
+    
+    has(value) {
+      return this.members.includes(value);
+    }
   }
 
-  let a 
+  let a = PGroup.empty.add("a");
+  let ab = a.add("b");
+  let b = ab.delete("a");
+
+  console.log(b.has("b"));
+  console.log(a.has("b"));
+  console.log(b.has("a"));
